e3e85e55f42fb9ff7b96a6f5544c435382682a2f
Aman Raj <160530800+rajamannn@users.noreply.github.com>
2024-10-27 21:31:43 +0530

    Add files via upload

---
+++ days of week.c
@@ -0,0 +1,34 @@
+//Days of the week
+
+#include<stdio.h>
+int main ()
+{
+ int day=4;
+  printf("Days of the week:  ");
+
+switch(day)
+  {
+  case 1:
+    printf("Monday");
+    break;
+  case 2:
+    printf("Tuesday");
+    break;
+  case 3:
+    printf("Wednesday");
+    break;
+  case 4:
+    printf("Thursday");
+    break;
+  case 5:
+    printf("Friday");
+    break;
+  case 6:
+    printf("Saturday");
+    break;
+  case 7:
+    printf("Sunday");
+    break;
+}
+ return 0;
+}
